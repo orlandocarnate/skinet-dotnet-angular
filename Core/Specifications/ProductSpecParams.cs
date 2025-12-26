fdb57de391736896b5c46b28d01f8d5ffe1577d5
Orlando Carnate <orlando.carnate@gmail.com>
2025-12-26 16:03:03 -0600

    product spec params; brands and types

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Core.Specifications;
+
+public class ProductSpecParams
+{
+    private List<string> _brands = [];
+    public List<string> Brands
+    {
+        get => _brands; // brands=boards,gloves
+        set 
+        { 
+            _brands = value.SelectMany(x => x.Split(',', 
+                StringSplitOptions.RemoveEmptyEntries))
+                .ToList();
+        }
+    }
+    
+    private List<string> _types = [];
+    public List<string> Types
+    {
+        get => _types; // types=boards,gloves
+        set 
+        { 
+            _types = value.SelectMany(x => x.Split(',', 
+                StringSplitOptions.RemoveEmptyEntries))
+                .ToList();
+        }
+    }
+
+    public string? Sort { get; set; }
+}
