23532969f5240f2af04df7c79cf77011a7297af0
Orlando Carnate <orlando.carnate@gmail.com>
2025-12-26 17:21:28 -0600

    add Search methods

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -40,4 +40,12 @@ public class ProductSpecParams
     }
 
     public string? Sort { get; set; }
+
+    private string _search;
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+    
 }
