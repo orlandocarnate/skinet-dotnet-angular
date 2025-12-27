68f1e734d0de29456f96fd11ef8fa0f0cb7cc01c
Orlando Carnate <orlando.carnate@gmail.com>
2025-12-27 12:41:43 -0600

    add http requests and some tailwind styles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,25 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient); // Depedency Injection alternative to contructor
+
   public title = 'SkiNET';
+
+  public products: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // http obervables automatically unsubscribes
+    this.http.get<any>(this.baseUrl + 'products').subscribe({
+      next: response => {
+        this.products = response.data;
+        console.log(response.data);
+      },
+      error: error => console.error(error),
+      complete: () => console.log(`completed`)
+    });
+  }
 }
